340f37cae4eba4a866f9d7502af3a67920baae85
William Meaton <will.meaton@gmail.com>
2015-12-03 10:18:07 +0000

    Do not make it 60FPS

--- src/ofApp.h
+++ src/ofApp.h
@@ -2,24 +2,14 @@
 
 #include "ofMain.h"
 #include "Keyboard.hpp"
-#include "GameState.hpp"
 #include "Texture.hpp"
 #include "World.hpp"
 #include "MenuState.hpp"
 
-
-
-//the aim of this project is to make a fluid-movement grid based survival game with multiple weapons, items, enemies and survival aspects.
-
 //So far I have made the base of a game but little is being displayed.
 //TODO
 /*
- -Get the World to make tiles and display them
- -Add more enmies to the game
  -Add transform movement - player is central with the map around them
- -Add colliders in the collider class
- -Add actual gameplay
- -Normalise the movement of the bullets/enemies
  -Other things
  */
 
@@ -33,7 +23,6 @@ class ofApp : public ofBaseApp{
 		void draw();
 		void keyPressed(int key);
 		void keyReleased(int key);
-        //two instances of the current states
-        //GameState gameState;
+        //instance of the current state
         MenuState menuState;
 };
